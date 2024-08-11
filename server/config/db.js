ed172013a66a0273ea26ddefdbc566bf7dd5ebd7
Ayus1123 <164751540+Ayus1123@users.noreply.github.com>
2024-08-11 23:23:16 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,10 @@
-const mongoose = require('mongoose');
-const connectDB = async () => {
-  
-  try {
-    mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Database Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-  }
-
-}
-
-module.exports = connectDB;
+const mongoose=require("mongoose"); 
+ mongoose.connect(process.env.MONGODB_URI)
+.then(()=> 
+{
+  console.log("connected");
+})
+.catch((e)=> 
+{
+    console.log("no")
+})
